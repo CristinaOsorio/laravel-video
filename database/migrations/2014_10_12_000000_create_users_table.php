e7eb66f19ea5aa9ad1bd297fa6b13399017f9d7e
CristinaOsorio <macry_08@hotmail.com>
2024-10-19 21:53:28 -0600

    feat: add nickname attribute  to users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('nickname');
             $table->string('surname')->nullable();
             $table->string('image')->nullable();
             $table->rememberToken();
