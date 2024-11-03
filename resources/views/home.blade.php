fa210a6149b8f1fcf15b3e529663c7aef8836648
CristinaOsorio <macry_08@hotmail.com>
2024-11-03 13:10:35 -0600

    style: moved video card loop outside of row div and added col-12 class to no videos alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,30 +8,30 @@
                 {{ session('message') }}
             </div>
         @endif
-        <div class="row">
-            @forelse ($videos as $video)
 
-                @include('partials.video-card', [
-                        'title' => $video->title,
-                        'image' => Storage::disk('images')->has($video->image) ? url('/miniatura/' . $video->image) : asset('images/videos/miniature-lg.png'),
-                        'videoId' => $video->id,
-                        'userId' => $video->user->id,
-                        'userNickname' => $video->user->nickname,
-                        'time' => \FormatTime::LongTimeFilter($video->created_at),
-                        'likes' => $video->likes_count,
-                        'dislikes' => $video->dislikes_count,
-                        'comments' => $video->comments_count,
-                        'menuItems' => []
-                    ])
-            @empty
-                <div class="alert alert-warning mt-3" role="alert">
-                    {{ trans('videos.no_videos') }}
-                </div>
-            @endforelse
+        @forelse ($videos as $video)
+            @include('partials.video-card', [
+                    'title' => $video->title,
+                    'image' => Storage::disk('images')->has($video->image) ? url('/miniatura/' . $video->image) : asset('images/videos/miniature-lg.png'),
+                    'videoId' => $video->id,
+                    'userId' => $video->user->id,
+                    'userNickname' => $video->user->nickname,
+                    'time' => \FormatTime::LongTimeFilter($video->created_at),
+                    'likes' => $video->likes_count,
+                    'dislikes' => $video->dislikes_count,
+                    'comments' => $video->comments_count,
+                    'menuItems' => []
+                ])
+        @empty
+        <div class="col-12">
+            <div class="alert alert-warning mt-3" role="alert">
+                {{ trans('videos.no_videos') }}
+            </div>
         </div>
+        @endforelse
         
         @include('partials.pagination', ['paginator' => $videos])
-        
+
     </div>
 </div>
 @endsection
